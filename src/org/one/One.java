4ee17811ec22f621ce47e441162cdcb4c18fb647
Manju <manjuvaliant@gmail.com>
2023-08-06 13:42:32 +0530

    2

--- src/org/one/One.java
+++ src/org/one/One.java
@@ -4,11 +4,8 @@ public class One {
 public static void main(String[] args) {
 	System.out.println("1");
 	System.out.println("2");
-//<<<<<<< HEAD
+	System.out.println("3");
 	System.out.println("4");
 	System.out.println("5");
-//=======
-	System.out.println("3");
-//>>>>>>> 6eede579d22b28d68dca4319f98730e2f3e76a39
 }
 }
